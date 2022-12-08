ce17c537cbd334522b99edf9c54d535e71271867
gkrls <gkrlsanj1@gmail.com>
2022-12-08 21:43:49 +0900

    commit

--- app/src/main/java/com/ouh/brailledetection/FirstFragment.kt
+++ app/src/main/java/com/ouh/brailledetection/FirstFragment.kt
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Matrix
+import android.media.ExifInterface
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
@@ -121,7 +122,7 @@ class FirstFragment : Fragment() {
             when (requestCode) {
                 REQUEST_IMAGE_CAPTURE -> {
                     var bitmap: Bitmap = BitmapFactory.decodeFile(imageFilePath)
-                    bitmap = rotate(bitmap, 90f)
+                    bitmap = rotate(bitmap, 90)!!
                     cameraViewModel.setImage(bitmap = bitmap)
                     try {
                         cameraViewModel.sendAddRequest()
@@ -151,6 +152,18 @@ class FirstFragment : Fragment() {
         }
     }
 
+    private fun exifOrientationToDegrees(exifOrientation: Int): Int {
+        if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            return 90
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            return 180
+        } else if (exifOrientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            return 270
+        }
+        return 0
+    }
+
+
     private fun sendTakePhotoIntent() {
         if (checkPermission(CAMERA_PERMISSION, REQUEST_IMAGE_CAPTURE)) {
             val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -177,11 +190,38 @@ class FirstFragment : Fragment() {
         }
     }
 
-    private fun rotate(bitmap: Bitmap, degree: Float): Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(degree)
-        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    //    private fun rotate(bitmap: Bitmap, degree: Float): Bitmap {
+//        val matrix = Matrix()
+//        matrix.postRotate(degree)
+//        return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+//    }
+    fun rotate(bitmap: Bitmap, degrees: Int): Bitmap { // 이미지 회전 및 이미지 사이즈 압축
+        var bitmap = bitmap
+        if (degrees != 0 && bitmap != null) {
+            val m = Matrix()
+            m.setRotate(
+                degrees.toFloat(), bitmap.width.toFloat() / 2,
+                bitmap.height.toFloat() / 2
+            )
+            try {
+                val converted = Bitmap.createBitmap(
+                    bitmap, 0, 0,
+                    bitmap.width, bitmap.height, m, true
+                )
+                if (bitmap != converted) {
+                    bitmap.recycle()
+                    bitmap = converted
+                    val options = BitmapFactory.Options()
+                    options.inSampleSize = 4
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 1280, 1280, true) // 이미지 사이즈 줄이기
+                }
+            } catch (ex: OutOfMemoryError) {
+                // 메모리가 부족하여 회전을 시키지 못할 경우 그냥 원본을 반환합니다.
+            }
+        }
+        return bitmap
     }
+
 
     override fun onDestroyView() {
         if (tts != null) {
